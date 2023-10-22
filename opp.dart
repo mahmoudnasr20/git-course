1e610f052b2f74f15b40bf569361c2e833d2334e
Mahmoud Nasr <alhawary11178@gmail.com>
2023-10-22 17:19:28 +0300

    add silverUser class

--- opp.dart
+++ opp.dart
@@ -5,15 +5,15 @@ class User {
   String password = "";
   int dob = 0;
 
-
   //constractor
-  void Enter_User_data( Name , username , pass , [dob=0]) {
+  void Enter_User_data(Name, username, pass, [dob = 0]) {
     name = Name;
     this.username = username;
     this.password = pass;
     this.dob = dob;
   }
-   User( name , username , pass , dob) {
+
+  User(name, username, pass, dob) {
     this.name = name;
     this.username = username;
     this.password = pass;
@@ -22,13 +22,10 @@ class User {
 }
 
 class primaryUser extends User {
-  
-  
-
 //  primaryUser(int nun):super("mahmoud","Ahmed41","ads44552a",54) {}......................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................
- 
- 
-  primaryUser(String name, String username, String pass, int dob) :super("mahmoud","Ahmed41","ads44552a",54){
+
+  primaryUser(String name, String username, String pass, int dob)
+      : super("mahmoud", "Ahmed41", "ads44552a", 54) {
     this.name = name;
     this.username = username;
     this.password = pass;
@@ -40,6 +37,8 @@ class primaryUser extends User {
     print("Hello ${this.name}, Welcome to our platform");
   }
 }
+
+class SilverUser {}
 
 void main() {
   primaryUser fist_user = new primaryUser("Ahmed", "ahmed1472", "da452da", 4);
